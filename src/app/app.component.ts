5426971922317d1eae8e67f4c5eb82a284e2006b
Alisson Lewinski <lissluc7@gmail.com>
2024-05-14 17:20:07 -0300

    base structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreditsComponent } from './credits/credits.component';
+import { ControlsComponent } from './controls/controls.component';
+import { DisplayComponent } from './display/display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CreditsComponent,
+    ControlsComponent,
+    DisplayComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/display/display.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+export interface ArtDisplayData {
+  title: string;
+  author: string;
+  rating: number;
+  year: string;
+  description: string;
+  imageUrl: string;
+  tags: string[];
+}
+
+@Component({
+  selector: 'app-display',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './display.component.html',
+  styleUrl: './display.component.scss'
+})
+export class DisplayComponent {
+
+  data: ArtDisplayData = {
+    title: 'The Way Of Kings',
+    author: 'Brandon Sanderson',
+    rating: 4.5,
+    year: '2010',
+    description: 'Roshar is a world of stone and storms. Uncanny tempests of incredible power sweep across the rocky terrain so frequently that they have shaped ecology and civilization alike. Animals hide in shells, trees pull in branches, and grass retracts into the soilless ground. Cities are built only where the topography offers shelter.',
+    imageUrl: 'https://m.media-amazon.com/images/I/91SkYC1TRHL._SY466_.jpg',
+    tags: ['Fantasy', 'Epic', 'Adventure']
+  }
+}
