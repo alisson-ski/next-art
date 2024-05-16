6d8d45a01d33eb15ff7169f8cac4b36da0f9a047
Alisson Lewinski <lissluc7@gmail.com>
2024-05-16 18:41:14 -0300

    text and stars

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -23,10 +23,13 @@ export class DisplayComponent {
   data: ArtDisplayData = {
     title: 'The Way Of Kings',
     author: 'Brandon Sanderson',
-    rating: 4.5,
+    rating: 4,
     year: '2010',
     description: 'Roshar is a world of stone and storms. Uncanny tempests of incredible power sweep across the rocky terrain so frequently that they have shaped ecology and civilization alike. Animals hide in shells, trees pull in branches, and grass retracts into the soilless ground. Cities are built only where the topography offers shelter.',
     imageUrl: 'https://m.media-amazon.com/images/I/91SkYC1TRHL._SY466_.jpg',
     tags: ['Fantasy', 'Epic', 'Adventure']
   }
+
+  filledStarsArray = new Array(this.data.rating).fill(0);
+  emptyStarsArray = new Array(5 - this.data.rating).fill(0);
 }
