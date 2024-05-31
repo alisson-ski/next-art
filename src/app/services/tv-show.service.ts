29177d222f9d1a05ff0d08474cc08aa111ae250b
Alisson Lewinski <lissluc7@gmail.com>
2024-05-31 19:51:10 -0300

    regret tvShowService inheritance

--- src/app/services/tv-show.service.ts
+++ src/app/services/tv-show.service.ts
@@ -1,14 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { ArtDisplayData } from '../interfaces/art-display-data';
 import { lastValueFrom, map } from 'rxjs';
 import { MovieService } from './movie.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { UtilityService } from './utility.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TvShowService extends MovieService {
+export class TvShowService {
 
-  override genreMap = new Map<number, string>([
+  http = inject(HttpClient);
+  utilityService = inject(UtilityService);
+
+  baseUrl = 'https://api.themoviedb.org/3';
+  apiKey = environment.apiKeys.movie;
+  brandImageConfig = {
+    url: 'assets/api-brands/tmdb.svg',
+    height: '0.7rem'
+  };
+
+  genreMap = new Map<number, string>([
     [10759, 'Action & Adventure'],
     [16, 'Animation'],
     [35, 'Comedy'],
@@ -26,17 +39,15 @@ export class TvShowService extends MovieService {
     [10768, 'War & Politics'],
     [37, 'Western']
   ]);
-
-  override variableFilters = [
+  
+  variableFilters = [
     {name: 'with_genres', values: Array.from(this.genreMap.keys())},
     {name: 'page', values: ['1']},
   ]
 
-  constructor() {
-    super();
-  }
+  constructor() { }
 
-  override getRandomArt(): Promise<ArtDisplayData> {
+  getRandomArt(): Promise<ArtDisplayData> {
     const params = this.getRandomQueryParams();
     return lastValueFrom(this.http.get(`${this.baseUrl}/discover/tv`, { params }).pipe(map((res: any) => {
       const item = this.utilityService.getRandomItemFromArray(res['results']);
@@ -44,7 +55,7 @@ export class TvShowService extends MovieService {
     })));
   }
 
-  override parseIntoArtDisplayData(tvShow: any): ArtDisplayData {
+  parseIntoArtDisplayData(tvShow: any): ArtDisplayData {
     return {
       title: tvShow.name,
       author: '',
@@ -54,5 +65,23 @@ export class TvShowService extends MovieService {
       imageUrl: `https://image.tmdb.org/t/p/w500${tvShow.poster_path}`,
       tags: tvShow.genre_ids.map((id: number) => this.genreMap.get(id))
     }
+  }
+
+  protected getRandomQueryParams() {
+    let params = this.getBaseParams()
+      .set('include_adult', 'true')
+      .set('sort_by', 'popularity.desc')
+      .set('language', 'en-US')
+
+    this.variableFilters.forEach(filter => {
+      const value = this.utilityService.getRandomItemFromArray(filter.values);
+      params = params.set(filter.name, value);
+    });
+
+    return params;
+  }
+
+  private getBaseParams() {
+    return new HttpParams().set('api_key', this.apiKey);
   }
 }
